fef1c70803d976f1ea20019eaf2a868d6d1d7046
mikyYun <mkyun2714@gmail.com>
2022-04-04 13:32:18 -0700

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("50", 50);
+assertEqual("11", "11");
+assertEqual("11", "33");
