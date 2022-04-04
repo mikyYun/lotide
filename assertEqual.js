a2759d843ef1938bcc111188d9b20ef301ed766c
mikyYun <mkyun2714@gmail.com>
2022-04-04 13:40:07 -0700

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Passed: $(actual) === $(expected)`);
   } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`Assertion Failed:  $(actual) !== $(expected)`);
   }
 };
 
