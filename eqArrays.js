b89d75df2ef0c7f5aa68b1bddf870b69c21cf206
mikyYun <mkyun2714@gmail.com>
2022-04-05 18:46:57 -0700

    eqArrays for checking arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`Assertion Passed`);
+  } else {
+    console.log(`Assertion Failed`);
+  }
+  console.log(actual, expected);
+};
+
+const eqArrays = (first, second) => {
+  let result = JSON.stringify(first) === JSON.stringify(second);
+  console.log(result);
+  return result;
+};
+eqArrays([1, 2, 3],[1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+eqArrays("HI", "HI");
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
