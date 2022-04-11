2e3fbc3860999dbbe78f98e82e0d52b2905fa8ec
mikyYun <mkyun2714@gmail.com>
2022-04-11 09:18:26 -0700

    first lotide refactor assertEqual

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual.js')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("50", 50);
+assertEqual("11", "11");
+assertEqual("11", "33");
+assertEqual("😁", "😜");
