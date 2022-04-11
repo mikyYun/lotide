5d6ed43098292da6a1e546aac94f0c97d5afe166
mikyYun <mkyun2714@gmail.com>
2022-04-11 09:22:10 -0700

    wrong function name update to tail

--- tail.js
+++ tail.js
@@ -1,10 +1,12 @@
-const assertEqual = function(test) {
+const tail = function(test) {
   let result = [];
   for (let i = 1; i < test.length; i ++) {
     result.push(test[i]);
   }
   console.log(result);
 };
-assertEqual([1, 2, 3, 4, 5, 6, 7]);
-assertEqual(["HI", "NEXT", "LAST"]);
-assertEqual([1, "string", "number", "who am I"]);
+tail([1, 2, 3, 4, 5, 6, 7]);
+tail(["HI", "NEXT", "LAST"]);
+tail([1, "string", "number", "who am I"]);
+
+module.exports = tail
