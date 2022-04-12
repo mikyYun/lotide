6198509a7480d84fa4a4d3b90086df312b47003d
mikyYun <mkyun2714@gmail.com>
2022-04-11 17:27:57 -0700

    index.js shorten combining keys and values as a variable name

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ const tail   = require('./tail');
 const middle = require('./middle');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
+
+// > _
+// undefined
+// > const _ = require('./index')
+// undefined
+// > _
+// {
+//   head: [Function: head],
+//   tail: [Function: tail],
+//   middle: [Function: middle]
+// }
+// > _.head
+// [Function: head]
+// > _.tail
+// [Function: tail]
+// > _.middle
+// [Function: middle]
+// > Object.values(_)
+// [ [Function: head], [Function: tail], [Function: middle] ]
